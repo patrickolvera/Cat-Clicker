b5325d65bf4bee8d240f9b4a2522a0b4ebca9384
Patrick Olvera <patrickolvera@gmail.com>
2018-08-14 19:18:29 -0500

    Feat: Add new cat functionality

--- js/app.v2.js
+++ js/app.v2.js
@@ -1,7 +1,11 @@
 (() => {
 
     const model = {
-    // All the names and urls of the cat images
+    /* TODO: Store cats in localStorage so that click counts are retained */
+    // Where the cat objects will be stored after init() is called
+        catObjs: {},
+    /* TODO: Add imagae attribution */
+    // The names and urls of the cat images
         catList : [
             ['Boots', 'images/cute-kitten.jpg'],
             ['Jimmy', 'images/cute-kitten-2.jpg'],
@@ -9,44 +13,34 @@
             ['Mittens', 'images/mittens.jpeg'],
             ['Pexels', 'images/pexels.jpg']
         ],
-        
-
-    // Creates cat objects with names, urls, and click counts
-        Cat : class {
-            constructor(arr){
-                this.name = arr[0],
-                this.url = arr[1],
-                this.clicks = 0
-            }    
-        },
-
-    // Creates and pushes cat objects to controller.catObjs
+    // Creates and pushes cat objects to catObjs
         init : () => {
             model.catList.forEach( cat => {
-                controller.catObjs[controller.strFormat(cat[0])] = new model.Cat(cat);
+                controller.newCat(cat);
             });
         }
-    
     }
 
     const menuView = {
     // Creates the featured cats based on catList
         init : () => {
             menuView.render()
-            this.catLinks = $('#cat-dropdown .dropdown-item').toArray();
+            const catLinkArr = $('#cat-dropdown .dropdown-item').toArray();
     
-            catLinks.forEach( link => {
+            catLinkArr.forEach( link => {
                 link.addEventListener('click', e => {
+    // Update DOM to show chosen cat
                     const objKey = e.target.getAttribute('href').replace('#', '');
 
-                    mainView.render(controller.catObjs[objKey]);
+                    mainView.render(controller.getCatObjs()[objKey]);
+                    adminView.render();
                 })
             })
         },
-
+    // Creates dropdown menu based on catList
         render : () => {
-            this.catList = controller.getCatList();
-            this.featured = $('#cat-dropdown')[0];
+            const catList = controller.getCatList();
+            const featured = $('#cat-dropdown')[0];
             featured.innerHTML = '';
             catList.forEach( cat => {
                 featured.innerHTML += 
@@ -59,7 +53,8 @@
     // Makes the counter element for the cat pic
     // A counter and name are added above each cat pic.
         init : () => {
-            mainView.render(controller.catObjs.boots); /* Cat loaded on Startup */
+            const initalCat = controller.getCatObjs()['boots']; /* Cat loaded on Startup */
+            mainView.render(initalCat); 
         },
 
         render : obj => {
@@ -78,30 +73,89 @@
     // When the image is clicked update that cat's number of clicks
     // then push it to the page          
             $('img').on('click', e => {
-                const objKey = controller.strFormat(e.target.getAttribute('name'));
-                controller.catObjs[objKey].clicks ++;
+                const key = controller.strFormat(e.target.getAttribute('name'));
+                controller.getCatObjs()[key].clicks ++;
 
                 const counter = $('.counter')[0];
-                counter.textContent = controller.catObjs[objKey].clicks;
+                counter.textContent = controller.getCatObjs()[key].clicks;
+            })
+        }
+    }
+
+    const adminView = {
+    // Show and hide admin controls with admin-btn
+        init : function () {
+            const adminBtn = $('#admin-btn');
+            const cancel = $('#cancel');
+            this.nameInput = $('#name');
+            this.urlInput = $('#url');
+
+            adminBtn.click(() => {
+                $('#admin-view').toggleClass('hidden');
+                $('#submit')[0].scrollIntoView();
+            });
+
+            cancel.click(() => {
+                $('#admin-view').addClass('hidden');
+            });
+    // Render this view (update the DOM elements with the right values)
+            this.render();
+    // Add event listener to submit button
+            this.handleSubmit();
+        },
+    // Placerholders set to current cat's properties
+        render : function () {
+            this.nameInput.val(controller.getCurCat().name);
+            this.urlInput.val(controller.getCurCat().url);
+        },
+    // Pushes the values in the inputs to the current cat object 
+        handleSubmit : function () {
+            $('#submit').on('click', e => {
+                e.preventDefault();
+    // Update array, create new object with name and image. Load on page
+    // TODO remove some arrow functions and use 'this' for sibling objects
+                const arr = [this.nameInput.val(), this.urlInput.val()];
+                const key = controller.strFormat(arr[0])
+
+                controller.spliceToList(arr)
+                controller.newCat(arr);
+                mainView.render(controller.getCatObjs()[key]);
+    // Update dropdown
+                menuView.init()
             })
         }
     }
 
     const controller = {
-
-    // Where the cat objects will be stored after init() is called
-    /* TODO: Store cats in localStorage so that click counts are retained */
-        catObjs: {},
+    // Creates cat objects with names, urls, and click counts
+        Cat : class {
+            constructor(arr){
+                this.name = arr[0],
+                this.url = arr[1],
+                this.clicks = 0
+            } 
+        },
 
         init : () => {
             model.init(),
             menuView.init(),
-            mainView.init()
+            mainView.init(),
+            adminView.init()
         },
+
+        getCatObjs : () => model.catObjs,
 
         getCatList : () => model.catList,
 
-        strFormat : str => str.toLowerCase().replace(/\s+/g, '')
+        spliceToList : arr => model.catList.splice(1, 0, arr),
+
+        getCurCat : () => model.catObjs[controller.strFormat($('img').attr('name'))],
+
+        strFormat : str => str.toLowerCase().replace(/\s+/g, ''),
+
+        newCat : function(cat) {
+            model.catObjs[controller.strFormat(cat[0])] = new this.Cat(cat);
+        },
     }
 
     controller.init();
